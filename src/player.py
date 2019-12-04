cbd0b8fdbe99ddfcc6e47b5cdd31f17bbf7b81ae
tania <tanislavens@yahoo.com>
2019-12-04 17:42:45 -0500

    wrote basic player class and room class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,21 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    '''
+    Player class with attributes: player_name, current_room
+    '''
+    def __init__(self, player_name, current_room):
+        self.player_name = player_name
+        self.current_room = current_room
+    
+    def get_player_name(self):
+        '''
+        Returns player_name
+        '''
+        return self.player_name
+    
+    def get_current_room(self):
+        '''
+        Returns current_room
+        '''
+        return self.current_room
--- src/room.py
+++ src/room.py
@@ -1,2 +1,22 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    '''
+    Room class with attributes: room_name, description
+    '''
+    def __init__(self, room_name, description):
+        self.room_name = room_name
+        self.description = description
+    
+    def get_room_name(self):
+        '''
+        Returns room_name
+        '''
+        return self.room_name
+    
+    def get_description(self):
+        '''
+        Returns description
+        '''
+        return self.description
